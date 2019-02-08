3b807a6747d705100b66d639d5fe846e6e14711e
Melsm650 <wp.developerhelpers@gmail.com>
2019-02-07 20:12:06 -0800

    Update - my json file and webpack.config

---
+++ client/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+class App extends React.Component {
+	constructor(){
+		super();
+	}
+
+	render() {
+		return(
+			<div>Hello mel</div>
+		);
+	}
+}
+
+export default App;
