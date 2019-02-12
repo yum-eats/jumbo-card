6c93b3c98552f2a7aa21e48b7e2a5ddb415dff51
Melsm650 <wp.developerhelpers@gmail.com>
2019-02-11 16:28:18 -0800

    Changed - Did a small clean up on method.js and seed.js

--- server/controllers/methods.js
+++ server/controllers/methods.js
@@ -1,9 +1,10 @@
 const db = require('../dbconnection/schema.js'); //accese to your scheema;
 
 exports.addData = (req,res) => {
-	db.create(req.body).then(results => res.send(results))
+	db.create(req.body).catch((err) => res.send(err))
 };
 exports.getData = (req,res) => {
 	db.findOne({restaurant_id: req.params.id})
-		.then(singleData => res.send(singleData));
+		.then(singleData => res.send(singleData))
+		.catch(err => res.send(err));
 };
