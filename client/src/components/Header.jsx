5a5273bde2092117205fd7ef421d0fc15029657c
Melsm650 <wp.developerhelpers@gmail.com>
2019-02-13 13:03:28 -0800

    Style- set for Header part for component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,54 @@
 import React from 'react';
 import star from '../images/icons/stars.png';
-
+import claimed from '../images/icons/checking.svg';
+import bigStar from '../images/icons/star.svg';
+import photo from '../images/icons/camera.svg';
+import share from '../images/icons/share-symbol.svg';
+import save from '../images/icons/bookmark-white.svg';
 const images = {
 	iconsStar: {
 		width: 132,
 		height: 24,
 		backgroundImage: `url(${star})`,
 		backgroundPosition: "0px -192px"
+	},
+	iconClaimed : {
+		width: 15,
+		height: 15,
+		backgroundImage: `url(${claimed})`,
+		display:"block",
+		backgroundSize: 'contain'
+	},
+	iconBigStar: {
+		width: 15,
+		height: 15,
+		backgroundImage: `url(${bigStar})`,
+		display:"block",
+		backgroundSize: 'contain'
+	},
+	iconPhoto : {
+		width: 20,
+		height: 20,
+		backgroundImage: `url(${photo})`,
+		display:"block",
+		backgroundSize: 'contain'
+	},
+	iconShare : {
+		width: 15,
+		height: 15,
+		backgroundImage: `url(${share})`,
+		display:"block",
+		backgroundSize: 'contain'
+	},
+	iconSave : {
+		width: 15,
+		height: 15,
+		backgroundImage: `url(${save})`,
+		display:"block",
+		backgroundSize: 'contain'
 	}
+
+
 };
 
 
@@ -22,7 +63,7 @@ class Header extends React.Component {
 				<div className="page-header-left">
 					<div className="title">
 						<h1>Daughter Thai Kitchen</h1>
-						<div className='claim-status'><span className='claimed-icon'></span> Claimed</div>
+						<div className='claim-status'><span className='claimed-icon' style={images.iconClaimed}></span> Claimed</div>
 					</div>
 					<div className='rating-info'>
 						<div className='rating-stars' style={images.iconsStar}></div>
@@ -39,12 +80,12 @@ class Header extends React.Component {
 				</div>
 
 				<div className="page-header-right">
-					<button> <span className="star-icon"></span> Write a Review</button>
+					<button> <span className="star-icon" style={images.iconBigStar}></span> Write a Review</button>
 					<div className="small-buttons">
 						<ul>
-							<li> <span className="photo-icon"></span> Add Photo</li>
-							<li><span className="share-icon"></span> Share</li>
-							<li><span className="sae-icon"></span> Save</li>
+							<li> <span className="photo-icon" style={images.iconPhoto}></span> Add Photo</li>
+							<li><span className="share-icon" style={images.iconShare}></span> Share</li>
+							<li><span className="sae-icon" style={images.iconSave}></span> Save</li>
 						</ul>
 					</div>
 				</div>
